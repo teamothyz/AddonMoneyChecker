ce972d4431c98afa839501aa9c363597d56dd10b
Tuan <zzzh0td0gzzz@gmail.com>
2023-07-02 03:12:09 +0700

    Add UI web app

---
+++ AddonMoney.WebApp/Pages/Balance/Index.cshtml.cs
@@ -0,0 +1,55 @@
+using AddonMoney.Data;
+using AddonMoney.Data.Models;
+using AddonMoney.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
+
+namespace AddonMoney.WebApp.Pages.Balance
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BalanceInfoRepository _balanceInfoRepository;
+        private readonly string _logPrefix = "[Balance-IndexModel]";
+
+        [BindProperty(SupportsGet = true)]
+        public int PageIndex { get; set; } = 1;
+
+        [BindProperty(SupportsGet = true)]
+        public int PageSize { get; set; } = 10;
+
+        [BindProperty(SupportsGet = true)]
+        public string NameFilter { get; set; } = null!;
+
+        [BindProperty(SupportsGet = true)]
+        public string IdFilter { get; set; } = null!;
+
+        public PaginatedList<BalanceInfo> BalanceInfos = null!;
+
+        public string Error { get; set; } = null!;
+
+        public IndexModel(BalanceInfoRepository balanceInfoRepository)
+        {
+            _balanceInfoRepository = balanceInfoRepository;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
+            {
+                int? id = null;
+                var validId = int.TryParse(IdFilter, out int idTemp);
+                if (validId) id = idTemp;
+                else IdFilter = null!;
+
+                BalanceInfos = await _balanceInfoRepository.GetBalanceInfos(id, NameFilter, PageIndex, PageSize);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"{_logPrefix} Got exception when getting balance list.", ex);
+                Error = ex.Message;
+            }
+            return Page();
+        }
+    }
+}
---
+++ AddonMoney.WebApp/Pages/Error/Index.cshtml.cs
@@ -0,0 +1,47 @@
+using AddonMoney.Data.Models;
+using AddonMoney.Data.Repositories;
+using AddonMoney.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
+
+namespace AddonMoney.WebApp.Pages.Error
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ErrorInfoRepository _errorInfoRepository;
+        private readonly string _logPrefix = "[Error-IndexModel]";
+
+        [BindProperty(SupportsGet = true)]
+        public int PageIndex { get; set; } = 1;
+
+        [BindProperty(SupportsGet = true)]
+        public int PageSize { get; set; } = 10;
+
+        [BindProperty(SupportsGet = true)]
+        public string HostFilter { get; set; } = null!;
+
+        public PaginatedList<ErrorInfo> ErrorInfos = null!;
+
+        public string Error { get; set; } = null!;
+
+        public IndexModel(ErrorInfoRepository errorInfoRepository)
+        {
+            _errorInfoRepository = errorInfoRepository;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
+            {
+                ErrorInfos = await _errorInfoRepository.GetErrors(HostFilter, PageIndex, PageSize);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"{_logPrefix} Got exception when getting errors list.", ex);
+                Error = ex.Message;
+            }
+            return Page();
+        }
+    }
+}
